c4d40578d8ff060870a71847c5ff585f7293667b
softlabxsanjeev1 <sanjeevyaadav28520@gmail.com>
2023-10-10 20:39:33 +0530

    first

---
+++ src/components/AboutContent.js
@@ -0,0 +1,26 @@
+import "./AboutContentStyles.css"
+import React from 'react'
+import { Link } from "react-router-dom"
+import resume from "../assets/Amit-Resume.pdf"
+import CodingProfile from "./Coding-Profile"
+
+const AboutContent = () => {
+  return (
+    <div className="about-section">
+    <div className="about">
+        <div className="intro">
+        <h1>Who am I?</h1>
+        <p>
+            I am a Web developer Looking for new oppurtunities to learn and grow my skills
+        </p>
+        <a href={resume} download className="btn">Download CV</a>
+        </div>
+    </div>
+    <CodingProfile/>
+    </div>
+    
+    
+  )
+}
+
+export default AboutContent
---
+++ src/components/Coding-Profile.js
@@ -0,0 +1,13 @@
+import "./Coding-ProfileStyles.css";
+import React from 'react'
+import {SiLeetcode,SiGeeksforgeeks} from "react-icons/si"
+import {FaHackerrank} from "react-icons/fa"
+function aboutdescribe() {
+  return (
+<>
+  
+</>
+  )
+}
+
+export default aboutdescribe
